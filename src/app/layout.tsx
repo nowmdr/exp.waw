eee4285fd310406ccb8b651e2c276e6921be7ca4
Jauhien <nowmdr@protonmail.com>
2026-02-06 14:28:20 +0100

    install GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Anton, Inter, Montserrat } from "next/font/google";
 
@@ -29,6 +30,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-3SVZZTS31Z"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3SVZZTS31Z');
+          `}
+        </Script>
+      </head>
       <body
         className={`${anton.variable} ${inter.variable} ${montserrat.variable}`}
       >
